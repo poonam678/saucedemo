6543fbd5b93152465733b3726228630441d48eda
poonam678 <poonam.patil.a@gmail.com>
2026-06-23 15:48:28 +0530

    Initial commit

---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+
+import {test} from "@playwright/test"
+ test("login",async({page})=>{
+
+await page.goto("https://www.saucedemo.com/")
+await page.locator('//input[@id="user-name"]').click()
+await page.locator('//input[@id="password"]').click()
+await page.locator('//input[@id="login-button"]').click()
+
+ })
