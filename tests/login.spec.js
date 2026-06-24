811107bb2f580541a3fa96bbe53c43059b6ee798
poonam678 <poonam.patil.a@gmail.com>
2026-06-24 14:55:33 +0530

    trigger

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,5 +6,6 @@ await page.goto("https://www.saucedemo.com/")
 await page.locator('//input[@id="user-name"]').click()
 await page.locator('//input[@id="password"]').click()
 await page.locator('//input[@id="login-button"]').click()
+//tring time trigger 
 
  })
